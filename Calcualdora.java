7a139b76d3848a3f2652db8b439ce7d75ad623bc
DavidG <igdrasilenllamas@gmail.com>
2016-01-21 10:59:35 +0100

    Añadido método sumValues que suma valores de 0 a 10

--- Calcualdora.java
+++ Calcualdora.java
@@ -23,12 +23,25 @@ public class Calcualdora
      */
     public void multiplesOfFive()
     {
-        int cont=6;
+        int cont = 6;
         while(cont < 95){
             if(cont % 5 == 0){
                 System.out.println(cont);
             }
             cont++;
         }
+    }
+    
+    /**
+     * Muestra la suma de números entre 0 y 10 estos incluidos
+     */
+    public void sumValues(){
+        int cont = 1;
+        int sum = 0;
+        while(cont <= 10){
+            sum+= cont;
+            cont++;
+        }
+        System.out.println(sum);
     }
 }
